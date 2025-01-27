f39f4a927b4dcab049c3b1c5c10d943de9fb4f5d
Esedov Muhammed <esedovmuhammed0@gmail.com>
2025-01-27 20:04:45 +0400

    feat: the project was created

---
+++ safir_gallery/src/main/java/app/cybergalaxy/safir_gallery/SafirGalleryApplication.java
@@ -0,0 +1,13 @@
+package app.cybergalaxy.safir_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafirGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafirGalleryApplication.class, args);
+	}
+
+}
---
+++ safir_gallery/src/test/java/app/cybergalaxy/safir_gallery/SafirGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package app.cybergalaxy.safir_gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafirGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
