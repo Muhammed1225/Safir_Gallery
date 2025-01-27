14a01aed8180f0dcedf8321b82613bef8b8a8c29
Esedov Muhammed <esedovmuhammed0@gmail.com>
2025-01-27 21:02:53 +0400

    feat: the login was completed

---
+++ safir_gallery/src/main/java/app/cybergalaxy/safir_gallery/controller/UserController.java
@@ -0,0 +1,17 @@
+package app.cybergalaxy.safir_gallery.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public void login() {}
+
+}
