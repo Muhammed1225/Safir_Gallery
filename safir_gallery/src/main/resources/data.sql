1344ab89e71ace3fcfd65205f1da30c9a27894d4
Esedov Muhammed <esedovmuhammed0@gmail.com>
2025-03-26 17:33:14 +0400

    feat: user updated

--- safir_gallery/src/main/resources/data.sql
+++ safir_gallery/src/main/resources/data.sql
@@ -6,15 +6,16 @@ values
 insert into users
 (username,password,enabled)
 values
-('codeAdam','{noop}Microsoft13',1),
+('Codeadam','{noop}345',1),
 ('admin1','{noop}1225',1);
 
 insert into authorities (username,authority) select 'admin1',role from authority_list where admin=true;
+insert into authorities (username,authority) select 'Codeadam',role from authority_list where admin=true;
 
 insert into admins
 (name,username)
 values
-('Ibrahim','codeAdam'),
+('Ibrahim','Codeadam'),
 ('Muhammed','admin1');
 
 insert into categories
